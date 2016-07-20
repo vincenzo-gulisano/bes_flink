9107df303875b2d1e24dae7c38b4a3e637c3431a
Vincenzo Gulisano <vinmas@chalmers.se>
2016-07-20 06:33:56 +0000

    added folder for cluster scripts

---
+++ cluster_scripts/sink.sh
@@ -0,0 +1,1 @@
+java bes_flink.BesReceiver --port 12346 --rateStatFile /home/odroid/bes_flink/data_donotversion/output_rate.csv --latencyStatFile /home/odroid/bes_flink/data_donotversion/output_latency.csv &
