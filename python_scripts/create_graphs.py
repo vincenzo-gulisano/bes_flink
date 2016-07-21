bb4f4305291e0e7f6a746288597e8ee45f56c22e
Vincenzo Massimiliano Gulisano <vincenzo.gulisano@chalmers.se>
2016-07-21 08:29:33 +0200

    Added python scripts to generate graphs and check results

---
+++ python_scripts/create_graphs.py
@@ -0,0 +1,56 @@
+from utils import read_file_adjust_timestamps_get_average_within_steady_state, \
+    read_file_adjust_timestamps_and_create_graph_time_value_simple, create_graph_multiple_time_value
+from statistics import mean
+
+base_folder = '/Users/vinmas/repositories/bes_flink/experiments/160721a/'
+
+input_rates = dict()
+throughputs = dict()
+
+keys = []
+
+for batch_size in [1, 2, 3, 4, 5]:
+
+    input_rates['batch' + str(batch_size)] = []
+    throughputs['batch' + str(batch_size)] = []
+    keys.append('batch' + str(batch_size))
+
+    for sleep_period in [100, 50, 25, 10, 5]:
+
+        input_rate_avgs = []
+        throughput_avgs = []
+
+        for repetition in [0, 1, 2, 3, 4]:
+            files_folder = 'exp_' + str(sleep_period) + '_' + str(batch_size) + '_' + str(repetition) + '/'
+
+            # # IF YOU WANT TO CREATE THE GRAPHS
+            # [input_rate_ts, input_rate_v] = read_file_adjust_timestamps_and_create_graph_time_value_simple(
+            #         base_folder + files_folder + 'input_rate.csv', 'Input rate', 'Time (s)', 'Input rate (t/s)',
+            #         base_folder + files_folder + 'input_rate.pdf',
+            # )
+            # [throughput_ts, throughput_v] = read_file_adjust_timestamps_and_create_graph_time_value_simple(
+            #         base_folder + files_folder + 'throughput.csv', 'Throughput', 'Time (s)', 'Throughput (t/s)',
+            #         base_folder + files_folder + 'throughput.pdf',
+            # )
+
+            # IF YOU JUST WANT THE DATA
+            input_rate_avgs.append(read_file_adjust_timestamps_get_average_within_steady_state(
+                    base_folder + files_folder + 'input_rate.csv', 60, 240))
+            throughput_avgs.append(read_file_adjust_timestamps_get_average_within_steady_state(
+                    base_folder + files_folder + 'throughput.csv', 60, 240))
+
+            # # This is for output rate and latency, not used yet
+            # read_file_adjust_timestamps_and_create_graph_time_value_simple(
+            #         base_folder + files_folder + 'output_rate.csv', 'Output rate', 'Time (s)', 'Output rate (t/s)',
+            #         base_folder + files_folder + 'output_rate.pdf',
+            # )
+            # read_file_adjust_timestamps_and_create_graph_time_value_simple(
+            #         base_folder + files_folder + 'output_latency.csv', 'Output latency', 'Time (s)', 'Latency (ms)',
+            #         base_folder + files_folder + 'output_latency.pdf',
+            # )
+
+        input_rates['batch' + str(batch_size)].append(mean(input_rate_avgs))
+        throughputs['batch' + str(batch_size)].append(mean(throughput_avgs))
+
+create_graph_multiple_time_value(input_rates, throughputs, keys, 'Scalability', 'Input rate (t/s)', 'Throughput(t/s)',
+                                 base_folder + 'scalability.pdf')
---
+++ python_scripts/utils.py
@@ -0,0 +1,110 @@
+from matplotlib import rcParams
+from matplotlib.backends.backend_pdf import PdfPages
+from statistics import mean
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def create_graph_time_value_simple(x, y, title, x_label, y_label, out_file):
+    rcParams.update({'figure.autolayout': True})
+    pp = PdfPages(out_file)
+
+    f = plt.figure()
+
+    plt.plot(x, y)
+
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.title(title)
+    plt.grid(True)
+    plt.close()
+
+    pp.savefig(f)
+    pp.close()
+
+    return
+
+
+def create_graph_multiple_time_value(xs, ys, keys, title, x_label, y_label, outFile):
+    markers = ['.', ',', 'o', 'v', '^', '<', '>', '1', '2', '3', '4', '8', 's', 'p', '*', 'h', 'H', '+', 'x', 'D', 'd',
+               '|', '_']
+
+    rcParams.update({'figure.autolayout': True})
+    pp = PdfPages(outFile)
+
+    f = plt.figure()
+    ax = plt.gca()
+
+    marker_i = 0
+    for key in keys:
+        plt.plot(xs[key], ys[key], label=key, marker=markers[marker_i])
+        marker_i = (marker_i + 1) % len(markers)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.title(title)
+    plt.grid(True)
+    plt.legend(loc='best')
+
+    ymin, ymax = plt.ylim()
+    plt.ylim(0, ymax)
+
+    plt.close()
+
+    pp.savefig(f)
+    pp.close()
+
+    return
+
+
+def read_file_simple(input_file):
+    t, v = np.genfromtxt(input_file, dtype=int, delimiter=',').transpose()
+    return t, v
+
+
+def read_file_and_adjust_timestamps(input_file, division_factor=1000):
+    [t, v] = read_file_simple(input_file)
+    t -= t[0]
+    t /= 1000
+    return t, v
+
+
+def read_file_adjust_timestamps_and_create_graph_time_value_simple(input_file, title, x_label, y_label, out_file,
+                                                                   division_factor=1000):
+    [t, v] = read_file_and_adjust_timestamps(input_file, division_factor)
+    create_graph_time_value_simple(t, v, title, x_label, y_label, out_file)
+    return t, v
+
+
+def read_file_adjust_timestamps_get_average_within_steady_state(input_file, steady_state_start, steady_state_end,
+                                                                division_factor=1000):
+    [t, v] = read_file_and_adjust_timestamps(input_file, division_factor)
+    return mean(v[steady_state_start:steady_state_end])
+
+# def create_graph_time_value(x, y, title, x_label, y_label, outFile, step=0):
+#     rcParams.update({'figure.autolayout': True})
+#     pp = PdfPages(outFile)
+#
+#     f = plt.figure()
+#     ax = plt.gca()
+#
+#     plt.plot(x, y)
+#
+#     if step > 0:
+#         counter = 1
+#         for i in range(int(x[0]) + step, int(x[-1]), step):
+#             plt.plot([i, i], [min(y), max(y)], color='r')
+#             indexes = [index for index, value in enumerate(x) if value >= i - step and value < i]
+#             plt.text(i, min(y), str(counter) + '\n' + str(int(statistics.median(y[indexes[0]:indexes[-1]]))),
+#                      verticalalignment='bottom', horizontalalignment='right', fontsize=7)
+#             counter += 1
+#
+#     plt.xlabel(x_label)
+#     plt.ylabel(y_label)
+#     plt.title(title)
+#     plt.grid(True)
+#     plt.close()
+#
+#     pp.savefig(f)
+#     pp.close()
+#
+#     return
