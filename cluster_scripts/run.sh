236de11cc772332d1ef8939046aae74d55b89a3d
Vincenzo Gulisano <vinmas@chalmers.se>
2016-07-21 06:30:26 +0000

    committing latest version of run.sh

--- cluster_scripts/run.sh
+++ cluster_scripts/run.sh
@@ -1,9 +1,55 @@
 
+for SLEEP_PERIOD in 100 50 25 10 5 2 1 0
+do
+for BATCH_SIZE in 1 2 3 4 5
+do
+for REPETITION in 0 1 2 3 4
+do
+
+EXP_FOLDER=/home/odroid/bes_flink/exps/exp_${SLEEP_PERIOD}_${BATCH_SIZE}_${REPETITION}
+echo "folder in which to store resulting files is ${EXP_FOLDER}"
+
+mkdir ${EXP_FOLDER}
+
 echo "Connecting to sink machine and starting sink"
 ssh 10.46.0.119 '/home/odroid/bes_flink/cluster_scripts/sink.sh'
-
 echo "Done..."
-sleep 10
+sleep 2
+
+echo "Connecting to injector machine and starting injector"
+ssh 10.46.0.119 '/home/odroid/bes_flink/cluster_scripts/injector.sh '"${SLEEP_PERIOD}"' '"${BATCH_SIZE}"' '
+echo "Done..."
+sleep 2
+
+echo "deploying query to flink"
+/home/odroid/flink/flink-1.0.3/bin/flink run -d -c bes_flink.BesOwnWin -p 1 /home/odroid/bes/bes_flink-0.0.1-SNAPSHOT.jar --boundX 1.0 --maxCons 19.0 --sinkIP 10.46.0.119 --sinkPort 12346 --injectorIP 10.46.0.119 --injectorPort 12345 --throughputStatFile /tmp/throughput.csv
+echo "Done..."
+sleep 2
+
+echo "waiting 5 minutes"
+sleep 300
+/home/odroid/flink/flink-1.0.3/bin/flink cancel bes
+echo "Done..."
+sleep 2
 
 echo "Connecting to sink machine and turning off everything"
 ssh 10.46.0.119 'pkill -9 java'
+
+echo "Connecting to injector  machine and turning off everything"
+ssh 10.46.0.119 'pkill -9 java'
+
+echo "Collecting files from sink machine"
+scp 10.46.0.119:/home/odroid/bes_flink/data_donotversion/output_rate.csv ${EXP_FOLDER}
+scp 10.46.0.119:/home/odroid/bes_flink/data_donotversion/output_latency.csv ${EXP_FOLDER}
+scp 10.46.0.119:/home/odroid/bes_flink/data_donotversion/sink.log ${EXP_FOLDER}
+
+echo "Collecting files from injector machine"
+scp 10.46.0.119:/home/odroid/bes_flink/data_donotversion/input_rate.csv ${EXP_FOLDER}
+scp 10.46.0.119:/home/odroid/bes_flink/data_donotversion/injector.log ${EXP_FOLDER}
+
+echo "Collecting files from slave machine"
+scp 10.46.0.114:/tmp/throughput.csv ${EXP_FOLDER}
+
+done
+done
+done
