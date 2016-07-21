b6fb3066ff0a7191d28e974f6afe3d6ac7c28724
Vincenzo Massimiliano Gulisano <vincenzo.gulisano@chalmers.se>
2016-07-21 14:11:04 +0200

    Split query into tasks

--- src/main/java/bes_flink/BesOwnWin.java
+++ src/main/java/bes_flink/BesOwnWin.java
@@ -11,6 +11,7 @@ import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.util.Collector;
 import org.slf4j.Logger;
@@ -75,8 +76,9 @@ public class BesOwnWin {
 		DataStreamSource<String> in = env
 				.socketTextStream(params.getRequired("injectorIP"),
 						params.getInt("injectorPort"));
-		in.flatMap(
-				new RichFlatMapFunction<String, Tuple4<Long, Long, Long, Double>>() {
+
+		SingleOutputStreamOperator<Tuple4<Long, Long, Long, Double>> conv = in
+				.flatMap(new RichFlatMapFunction<String, Tuple4<Long, Long, Long, Double>>() {
 
 					SimpleDateFormat sdf = new SimpleDateFormat(
 							"yyyy-MM-dd HH:mm:ss");
@@ -130,81 +132,73 @@ public class BesOwnWin {
 						}
 
 					}
-				})
-				.flatMap(
-						new RichFlatMapFunction<Tuple4<Long, Long, Long, Double>, Tuple4<Long, Long, Long, Double>>() {
+				});
 
-							Aggregate<Tuple4<Long, Long, Long, Double>, Tuple4<Long, Long, Long, Double>> aggregate;
+		SingleOutputStreamOperator<Tuple4<Long, Long, Long, Double>> agg = conv
+				.flatMap(new RichFlatMapFunction<Tuple4<Long, Long, Long, Double>, Tuple4<Long, Long, Long, Double>>() {
 
-							public void open(Configuration parameters)
-									throws Exception {
-								aggregate = new Aggregate<Tuple4<Long, Long, Long, Double>, Tuple4<Long, Long, Long, Double>>(
-										1000L * 60L * 60L * 24L,
-										1000L * 60L * 60L * 24L,
-										new BesWindow());
-							}
+					Aggregate<Tuple4<Long, Long, Long, Double>, Tuple4<Long, Long, Long, Double>> aggregate;
 
-							@Override
-							public void flatMap(
-									Tuple4<Long, Long, Long, Double> value,
-									Collector<Tuple4<Long, Long, Long, Double>> out)
-									throws Exception {
-								List<Tuple4<Long, Long, Long, Double>> result = aggregate
-										.processTuple(value);
-								for (Tuple4<Long, Long, Long, Double> t : result)
-									out.collect(t);
+					public void open(Configuration parameters) throws Exception {
+						aggregate = new Aggregate<Tuple4<Long, Long, Long, Double>, Tuple4<Long, Long, Long, Double>>(
+								1000L * 60L * 60L * 24L,
+								1000L * 60L * 60L * 24L, new BesWindow());
+					}
 
-							}
-						})
-				.flatMap(
-						new RichFlatMapFunction<Tuple4<Long, Long, Long, Double>, Tuple4<Long, Long, Long, Double>>() {
+					@Override
+					public void flatMap(Tuple4<Long, Long, Long, Double> value,
+							Collector<Tuple4<Long, Long, Long, Double>> out)
+							throws Exception {
+						List<Tuple4<Long, Long, Long, Double>> result = aggregate
+								.processTuple(value);
+						for (Tuple4<Long, Long, Long, Double> t : result)
+							out.collect(t);
 
-							boolean boundValues = false;
-							double bound = 0;
-							double maxCons = 0;
-							private Random r;
+					}
+				});
 
-							public void open(Configuration parameters)
-									throws Exception {
-								ParameterTool params = (ParameterTool) getRuntimeContext()
-										.getExecutionConfig()
-										.getGlobalJobParameters();
-								if (params.has("bound")) {
-									boundValues = true;
-									bound = Double.valueOf(params.get("bound"));
-									LOG.info("Set bound to " + bound);
-								}
-								maxCons = params.getDouble("maxCons");
-								r = new Random();
-							}
+		SingleOutputStreamOperator<Tuple4<Long, Long, Long, Double>> map = agg
+				.flatMap(new RichFlatMapFunction<Tuple4<Long, Long, Long, Double>, Tuple4<Long, Long, Long, Double>>() {
 
-							private double laplace(double lambda) {
-								double u = r.nextDouble() * -1 + 0.5;
-								return -1
-										* (lambda * Math.signum(u) * Math
-												.log(1 - 2 * Math.abs(u)));
-							}
+					boolean boundValues = false;
+					double bound = 0;
+					double maxCons = 0;
+					private Random r;
 
-							@Override
-							public void flatMap(
-									Tuple4<Long, Long, Long, Double> value,
-									Collector<Tuple4<Long, Long, Long, Double>> out)
-									throws Exception {
+					public void open(Configuration parameters) throws Exception {
+						ParameterTool params = (ParameterTool) getRuntimeContext()
+								.getExecutionConfig().getGlobalJobParameters();
+						if (params.has("bound")) {
+							boundValues = true;
+							bound = Double.valueOf(params.get("bound"));
+							LOG.info("Set bound to " + bound);
+						}
+						maxCons = params.getDouble("maxCons");
+						r = new Random();
+					}
 
-								double noise = laplace(boundValues ? bound
-										: maxCons);
-								out.collect(new Tuple4<Long, Long, Long, Double>(
-										value.f0, value.f1, value.f2, value.f3
-												+ noise));
+					private double laplace(double lambda) {
+						double u = r.nextDouble() * -1 + 0.5;
+						return -1
+								* (lambda * Math.signum(u) * Math
+										.log(1 - 2 * Math.abs(u)));
+					}
 
-							}
+					@Override
+					public void flatMap(Tuple4<Long, Long, Long, Double> value,
+							Collector<Tuple4<Long, Long, Long, Double>> out)
+							throws Exception {
 
-						})
-				.addSink(
-						new SinkSocket(params.getRequired("sinkIP"), params
-								.getInt("sinkPort")));
+						double noise = laplace(boundValues ? bound : maxCons);
+						out.collect(new Tuple4<Long, Long, Long, Double>(
+								value.f0, value.f1, value.f2, value.f3 + noise));
 
-		env.setParallelism(1);
+					}
+
+				});
+
+		map.addSink(new SinkSocket(params.getRequired("sinkIP"), params
+				.getInt("sinkPort")));
 
 		env.execute("bes");
 
