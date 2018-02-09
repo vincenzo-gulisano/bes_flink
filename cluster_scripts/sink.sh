39664ede7f577cdcb46055387aa3937f5e0c8a3b
Vincenzo Gulisano <vincenzo.gulisano@chalmers.se>
2018-02-09 19:28:00 +0000

    Fixed bug in script

--- cluster_scripts/sink.sh
+++ cluster_scripts/sink.sh
@@ -1,4 +1,4 @@
 
 OUT_FILE=/home/vincenzo/bes_flink/data_donotversion/sink.log
 
-java -cp target/fat.bes_flink-0.0.1-SNAPSHOT.jar bes_flink.BesReceiver --port 12346 --rateStatFile /home/vincenzo/bes_flink/data_donotversion/output_rate.csv --latencyStatFile /home/odroid/bes_flink/data_donotversion/output_latency.csv &>${OUT_FILE} &
+java -cp /home/vincenzo/bes_flink/target/fat.bes_flink-0.0.1-SNAPSHOT.jar bes_flink.BesReceiver --port 12346 --rateStatFile /home/vincenzo/bes_flink/data_donotversion/output_rate.csv --latencyStatFile /home/odroid/bes_flink/data_donotversion/output_latency.csv &>${OUT_FILE} &
