e0325ddf330999adde095fa9a6caa32916dc3554
Vincenzo Gulisano <vincenzo.gulisano@chalmers.se>
2018-02-09 19:12:48 +0000

    fixing scripts for these new experiments. pom2.xml to compile a fat jar

--- cluster_scripts/injector.sh
+++ cluster_scripts/injector.sh
@@ -6,5 +6,5 @@ OUT_FILE=/home/vincenzo/bes_flink/data_donotversion/injector.log
 echo "sleep period: ${SLEEP_PERIOD}" > ${OUT_FILE}
 echo "batch size: ${BATCH_SIZE}" >> ${OUT_FILE}
 
-java bes_flink.BesInjector --rateStatFile /home/odroid/bes_flink/data_donotversion/input_rate.csv --port 12345 --input /home/odroid/bes_flink/data_donotversion/sm1000.sorted.txt --sleepPeriod ${SLEEP_PERIOD} --batchSize ${BATCH_SIZE} &>>${OUT_FILE} &
+java bes_flink.BesInjector --rateStatFile /home/vincenzo/bes_flink/data_donotversion/input_rate.csv --port 12345 --input /home/odroid/bes_flink/data_donotversion/sm1000.sorted.txt --sleepPeriod ${SLEEP_PERIOD} --batchSize ${BATCH_SIZE} &>>${OUT_FILE} &
 
